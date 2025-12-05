51c3389d5b0c05379141205c389c9b63effccd72
Jakub Prabucki <jakub.prabucki@gmail.com>
2025-12-05 07:27:49 +0100

    Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,18 +1,49 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var result = 0L
+        var start = 50
+
+        for (line in input) {
+            val c = line[0]
+            val i = line.substring(1).toInt()
+            Math.floorMod(i, 100)
+
+            start = if (c == 'R') {
+                Math.floorMod(start + i, 100)
+            } else {
+                Math.floorMod(start - i, 100)
+            }
+
+            if (start == 0) {
+                result++
+            }
+        }
+
+        return result.toInt()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var result2 = 0L
+        var start = 50
+
+        for (line in input) {
+            val c = line[0]
+            val i = line.substring(1).toInt()
+
+            result2 += (i / 100)
+            val x = Math.floorMod(i, 100)
+
+            if (c == 'R') {
+                if (start + x >= 100) result2++
+                start = Math.floorMod(start + i, 100)
+            } else {
+                if (start - x <= 0 && start != 0) result2++
+                start = Math.floorMod(start - i, 100)
+            }
+        }
+
+        return result2.toInt()
     }
-
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
